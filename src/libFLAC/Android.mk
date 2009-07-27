8c21f4f34a92417fdf080f7b84b9395a8d355f96
Kenny Root <kenny@the-b.org>
2009-07-27 07:01:36 -0500

    Add Android-specific build files for libFLAC

---
+++ src/libFLAC/Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	bitmath.c \
+	bitreader.c \
+	bitwriter.c \
+	cpu.c \
+	crc.c \
+	fixed.c \
+	float.c \
+	format.c \
+	lpc.c \
+	md5.c \
+	memory.c \
+	metadata_iterators.c \
+	metadata_object.c \
+	stream_decoder.c \
+	stream_encoder.c \
+	stream_encoder_framing.c \
+	window.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/../.. \
+	$(LOCAL_PATH)/../../include
+
+LOCAL_CFLAGS += -DHAVE_CONFIG_H
+#LOCAL_CFLAGS  += -D_REENTRANT -DPIC -DU_COMMON_IMPLEMENTATION -fPIC 
+LOCAL_CFLAGS += -O3 -funroll-loops -finline-functions
+
+LOCAL_LDLIBS += -lm
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_MODULE := libFLAC
+
+include $(BUILD_SHARED_LIBRARY)
