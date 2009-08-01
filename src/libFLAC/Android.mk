6b632e29a7ca8d99e81bdba2f1a3a6b993de4dd1
Kenny Root <kenny@the-b.org>
2009-07-31 21:13:40 -0500

    Remove unneeded source files (saves approx 130k)

--- src/libFLAC/Android.mk
+++ src/libFLAC/Android.mk
@@ -4,17 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
 	bitmath.c \
 	bitreader.c \
-	bitwriter.c \
 	cpu.c \
 	crc.c \
 	fixed.c \
 	float.c \
 	format.c \
 	lpc.c \
-	md5.c \
 	memory.c \
-	metadata_iterators.c \
-	metadata_object.c \
 	stream_decoder.c \
 	window.c
 
@@ -23,7 +19,7 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../.. \
 	$(LOCAL_PATH)/../../include
 
-LOCAL_CFLAGS += -DHAVE_CONFIG_H
+LOCAL_CFLAGS += -DHAVE_CONFIG_H -DFLAC__NO_MD5 -DFLAC__INTEGER_ONLY_LIBRARY
 LOCAL_CFLAGS += -D_REENTRANT -DPIC -DU_COMMON_IMPLEMENTATION -fPIC
 LOCAL_CFLAGS += -O3 -funroll-loops -finline-functions
 
